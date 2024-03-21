389e3cd77e9a7acb8c1983d4cebb04eb0972d644
katerine bonilla <99689515+katerinebonilla@users.noreply.github.com>
2024-03-20 23:30:16 -0400

    Update PizzaBoxAnalysis.sql

--- SQL/Pizzabox/PizzaBoxAnalysis.sql
+++ SQL/Pizzabox/PizzaBoxAnalysis.sql
@@ -1,32 +1,42 @@
+/* 
+Pizza Box Stats
+*/
 SELECT *
 FROM pizza_sales
+	
 -- Finding the total Revenue of all pizza orders
 SELECT 
 	SUM(total_price) AS total_revenue
 FROM pizza_sales
+	
 -- Finding the Average Order Value 
 SELECT 
 	(SUM(total_price)/COUNT(DISTINCT order_id)) AS avg_order_value
 FROM pizza_sales
+	
 -- Finding the total pizzas sold
 SELECT 
 	  SUM(quantity) AS total_pizzas_sold
 FROM pizza_sales
--- Finding the total orders 
+	
+-- Finding the total number of orders 
 SELECT 
 	COUNT(DISTINCT order_id) AS total_orders
 FROM pizza_sales
+	
 -- Finding Average Pizzas Per Order
 SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
 CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
 AS avg_pizzas_per_order
 FROM pizza_sales
+	
 -- Orders Per Day 
 SELECT 
 	DATENAME(DW,order_date) AS day_name,
 	COUNT(DISTINCT order_id) AS total_orders
 FROM pizza_sales
 GROUP BY DATENAME(DW,order_date) 
+	
 -- Orders Per Hour 
 SELECT 
 	DATEPART(HOUR,order_time) AS order_hour,
@@ -34,6 +44,7 @@ SELECT
 FROM pizza_sales
 GROUP BY DATEPART(HOUR,order_time)
 ORDER BY DATEPART(HOUR,order_time)
+	
 --Percentage of Sales Per Pizza Category
 SELECT 
 	pizza_category,CAST(SUM(total_price) AS DECIMAL (10,2)) AS total_revenue,
@@ -47,12 +58,14 @@ SELECT
 FROM pizza_sales
 GROUP BY pizza_size
 ORDER BY percent_size DESC
+	
 --Total Pizzas Sold by Pizza Category
 SELECT 
 	pizza_category,
 	SUM(quantity) as pizzas_sold
 FROM pizza_sales
 GROUP BY pizza_category
+	
 --Total 5 Best Sellers by Total Pizzas Sold
 SELECT 
 	TOP 5 pizza_name, 
@@ -60,6 +73,7 @@ SELECT
 FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY pizzas_sold DESC
+	
 -- Bottom 5 Best Sellers By Total Pizzas Sold
 SELECT 
 	TOP 5 pizza_name, 
